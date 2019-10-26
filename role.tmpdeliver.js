0e38287012176e26dafacac9c099dc9f319abed7
Carson McManus <carson.mcmanus1@gmail.com>
2019-10-26 01:01:02 -0400

    make tmpdeliver not leave iff deposit target is full

--- role.tmpdeliver.js
+++ role.tmpdeliver.js
@@ -18,24 +18,26 @@ let roleTmpDelivery = {
 
 		let withdrawTarget = Game.getObjectById(creep.memory.withdrawTargetId);
 		let depositTarget = Game.getObjectById(creep.memory.depositTargetId);
-		
+
 		if (!withdrawTarget) {
-		    delete creep.memory.withdrawTargetId;
-		    console.log(creep.name, "Can't find withdraw target");
-		    creep.say("help");
-		    return;
+			delete creep.memory.withdrawTargetId;
+			console.log(creep.name, "Can't find withdraw target");
+			creep.say("help");
+			return;
 		}
 		if (!depositTarget) {
-		    delete creep.memory.depositTargetId;
-		    console.log(creep.name, "Can't find deposit target");
-		    creep.say("help");
-		    return;
+			delete creep.memory.depositTargetId;
+			console.log(creep.name, "Can't find deposit target");
+			creep.say("help");
+			return;
 		}
 
 		if (creep.memory.delivering) {
 			if (creep.pos.isNearTo(depositTarget)) {
-				creep.transfer(depositTarget, RESOURCE_ENERGY);
-				creep.memory.delivering = false;
+				let transferResult = creep.transfer(depositTarget, RESOURCE_ENERGY);
+				if (transferResult !== ERR_FULL) {
+					creep.memory.delivering = false;
+				}
 			}
 			else {
 				creep.travelTo(depositTarget, {visualizePathStyle:{}});
