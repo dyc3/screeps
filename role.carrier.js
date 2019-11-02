ba441f7748e39a10f09c6f59c48497d673b56f7f
Carson McManus <carson.mcmanus1@gmail.com>
2019-11-02 11:14:04 -0400

    allow carrier depositTarget to be overridden manually

--- role.carrier.js
+++ role.carrier.js
@@ -6,15 +6,10 @@ let util = require('util');
 let roleCarrier = {
 	/** @param {Creep} creep **/
 	run: function(creep) {
-		// TEMP set harvest target for testing
-		// creep.memory.harvestTarget = {
-		// 	id: "55c34a6b5be41a0a6e80c5b5",
-		// 	x: 29,
-		// 	y: 43,
-		// 	roomName: "W15N9",
-		// 	harvestPos: { x: 30, y: 44 },
-		// };
-		creep.memory.depositTarget = "5b843ec8f656bb71dce8978d";
+		// TEMP set deposit target for testing
+		if (!creep.memory.depositTarget) {
+		    creep.memory.depositTarget = "5b843ec8f656bb71dce8978d";
+		}
 
 		if (!creep.memory.harvestTarget) {
 			console.log(creep.name, "ERR: no harvest target, this needs to be assigned by a job (similar to how relays are assigned)");
