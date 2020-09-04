1cb1779752db9ff893dcd887d0b06872980b990d
Carson McManus <carson.mcmanus1@gmail.com>
2020-09-04 03:01:37 -0400

    fix something i dont remember

--- src/role.testlogistics.js
+++ src/role.testlogistics.js
@@ -1,6 +1,7 @@
 let traveler = require("traveler");
 let util = require("util");
 let brainLogistics = require("brain.logistics");
+let taskDepositMaterials = require("task.depositmaterials");
 
 module.exports = {
 	run(creep) {
@@ -32,15 +33,26 @@ module.exports = {
 
 		creep.log("Delivery task:", JSON.stringify(deliveryTask));
 
+		// HACK: deposit materials if the resources held don't match
+		if (creep.store.getFreeCapacity(deliveryTask.resource) + creep.store.getUsedCapacity(deliveryTask.resource) !== creep.store.getCapacity()) {
+			taskDepositMaterials.run(creep, exclude_energy=false);
+			return;
+		}
+
+		if (creep.memory.delivering && creep.store.getUsedCapacity(deliveryTask.resource) === 0) {
+			creep.memory.delivering = false;
+		}
+
 		if (creep.memory.delivering) {
 			if (deliveryTask.sink.object) {
 				if (creep.pos.isNearTo(deliveryTask.sink.object)) {
-					if (creep.transfer(deliveryTask.sink.object, deliveryTask.resource) === OK) {
+					let result = creep.transfer(deliveryTask.sink.object, deliveryTask.resource);
+					if (result === OK) {
 						deliveryTask.registerDelivery(creep.store.getCapacity());
 						creep.memory.delivering = false;
 					}
 					else {
-						creep.log("FAILED TO TRANSFER");
+						creep.log(`FAILED TO TRANSFER: ${result}`);
 					}
 				}
 				else {
