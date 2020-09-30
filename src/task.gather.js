7dccc63b02c7a2a0d023e88be74969512e53d29d
“Carson <carson.mcmanus1@gmail.com>
2020-09-30 15:19:27 -0400

    rework task.gather to use global logistics sources

--- src/task.gather.js
+++ src/task.gather.js
@@ -1,9 +1,15 @@
 let traveler = require('traveler');
 let util = require('util');
 let toolEnergySource = require('tool.energysource');
+let brainLogistics = require("brain.logistics");
 
 let taskGather = {
-	run: function(creep) {
+	/**
+	 *
+	 * @param {Creep} creep
+	 * @deprecated
+	 */
+	run_old(creep) {
 
 		if (!creep.room.storage || (creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] < 100000)) {
 			// Pick up dropped resources
@@ -197,7 +203,80 @@ let taskGather = {
 				}
 			}
 		}
-	}
+	},
+
+	getGatherTarget(creep) {
+		let gatherTarget;
+		if (creep.memory.gatherTarget) {
+			gatherTarget = Game.getObjectById(creep.memory.gatherTarget);
+			if (gatherTarget && (!gatherTarget.store || gatherTarget.store.getUsedCapacity(RESOURCE_ENERGY) > 0)) {
+				return gatherTarget;
+			}
+			else {
+				delete creep.memory.gatherTarget;
+				gatherTarget = null;
+			}
+		}
+
+		let sources = brainLogistics.findSources({
+			resource: RESOURCE_ENERGY,
+			roomName: creep.memory.targetRoom,
+		});
+		sources = _.sortByOrder(sources, [
+			s => creep.pos.getRangeTo(s.object),
+		],
+		["asc"]);
+
+		if (sources.length > 0) {
+			let selectedSource = _.first(sources);
+			creep.memory.gatherTarget = selectedSource.objectId;
+			return selectedSource.object;
+		}
+		else if (creep.getActiveBodyparts(WORK) > 0) {
+			let spawn = util.getSpawn(creep.room);
+			let haveContainer = spawn.room.find(FIND_STRUCTURES, {
+				filter: struct => struct.structureType === STRUCTURE_CONTAINER || struct.structureType === STRUCTURE_STORAGE,
+			}).length > 0;
+			if (!spawn || !haveContainer || creep.room.controller.level <= 3 || !creep.room.storage || creep.room.storage.store[RESOURCE_ENERGY] <= 0) {
+				let source = creep.pos.findClosestByPath(FIND_SOURCES, {
+					filter: s => s.energy > 0,
+				});
+				creep.memory.gatherTarget = source.id;
+				return source;
+			}
+		}
+	},
+
+	/**
+	 * Gather energy for a creep to do work.
+	 * @param {Creep} creep
+	 */
+	run(creep) {
+		if (!creep.memory._gatherLastRun || Game.time - 1 > creep.memory._gatherLastRun) {
+			delete creep.memory.gatherTarget;
+		}
+		let gatherTarget = this.getGatherTarget(creep);
+
+		if (creep.pos.isNearTo(gatherTarget)) {
+			if (gatherTarget instanceof Source) {
+				creep.harvest(gatherTarget);
+			}
+			else if (gatherTarget instanceof Resource) {
+				creep.pickup(gatherTarget);
+			}
+			else if (gatherTarget.store) {
+				creep.withdraw(gatherTarget, RESOURCE_ENERGY);
+			}
+			else {
+				creep.log(`Don't know how to grab ${gatherTarget}`);
+			}
+		}
+		else {
+			creep.travelTo(gatherTarget);
+		}
+
+		creep.memory._gatherLastRun = Game.time;
+	},
 }
 
 module.exports = taskGather;
