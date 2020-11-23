0ca7f1ee93aa4aaa811f26653728d8eeda3ebb5b
Carson McManus <carson.mcmanus1@gmail.com>
2020-11-23 10:00:15 -0500

    refactor findClosestOwnedRooms to be easier to read

--- src/util.js
+++ src/util.js
@@ -72,23 +72,30 @@ let util = {
 				}
 				return true;
 			});
-		rooms.sort((a, b) => {
-			let roomDistance = Game.map.getRoomLinearDistance(targetPos.roomName, a.name) - Game.map.getRoomLinearDistance(targetPos.roomName, b.name);
-			if (roomDistance !== 0) {
-				return roomDistance;
-			}
-			let roomPathA = Game.map.findRoute(targetPos.roomName, a.name);
-			let roomPathB = Game.map.findRoute(targetPos.roomName, b.name);
-			let roomPathDistance = roomPathA.length - roomPathB.length;
-			if (roomPathDistance !== 0) {
-				return roomPathDistance;
-			}
-			if (roomPathA[0].room === roomPathB[0].room) {
-				return 0;
-			}
+		// Old sort, just in case i need it
+		// rooms.sort((a, b) => {
+		// 	let roomDistance = Game.map.getRoomLinearDistance(targetPos.roomName, a.name) - Game.map.getRoomLinearDistance(targetPos.roomName, b.name);
+		// 	if (roomDistance !== 0) {
+		// 		return roomDistance;
+		// 	}
+		// 	let roomPathA = Game.map.findRoute(targetPos.roomName, a.name);
+		// 	let roomPathB = Game.map.findRoute(targetPos.roomName, b.name);
+		// 	let roomPathDistance = roomPathA.length - roomPathB.length;
+		// 	if (roomPathDistance !== 0) {
+		// 		return roomPathDistance;
+		// 	}
+		// 	if (roomPathA[0].room === roomPathB[0].room) {
+		// 		return 0;
+		// 	}
 
-			return PathFinder.search(targetPos, { pos: new RoomPosition(25, 25, roomPathA[0].room), range: 20 }).path.length - PathFinder.search(targetPos, { pos: new RoomPosition(25, 25, roomPathB[0].room), range: 20 }).path.length;
-		});
+		// 	return PathFinder.search(targetPos, { pos: new RoomPosition(25, 25, roomPathA[0].room), range: 20 }).path.length - PathFinder.search(targetPos, { pos: new RoomPosition(25, 25, roomPathB[0].room), range: 20 }).path.length;
+		// });
+		rooms = _.sortByAll(rooms, ...[
+			r => Game.map.getRoomLinearDistance(targetPos.roomName, r.name),
+			r => Game.map.findRoute(targetPos.roomName, r.name).length,
+			// TODO: use traveler.Traveler.findTravelPath instead
+			r => PathFinder.search(targetPos, { pos: new RoomPosition(25, 25, r.name), range: 20 }).path.length,
+		]);
 		return rooms;
 	},
 
@@ -300,7 +307,7 @@ let util = {
 
 	getCreeps(...roles) {
 		if (roles.length > 0) {
-		return _.filter(Game.creeps, creep => roles.includes(creep.memory.role));
+			return _.filter(Game.creeps, creep => roles.includes(creep.memory.role));
 		}
 		else {
 			return Game.creeps;
