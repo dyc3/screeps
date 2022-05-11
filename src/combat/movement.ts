537054bd4409acf0fc17caec72295ac30b398a8a
Carson McManus <carson.mcmanus1@gmail.com>
2022-05-11 10:43:32 -0400

    add naive unitCohesion method

--- src/combat/movement.ts
+++ src/combat/movement.ts
@@ -18,3 +18,13 @@ export function getAveragePosition(creeps: Creep[]): RoomPosition {
 	const xy = creeps.map(c => [c.pos.x, c.pos.y] as [number, number]).reduce((a, b) => [a[0] + b[0], a[1] + b[1]]);
 	return new RoomPosition(xy[0] / creeps.length, xy[1] / creeps.length, roomName);
 }
+
+/**
+ * A score indicating how close by the given creeps are.
+ * @param creeps
+ */
+export function unitCohesion(creeps: Creep[]): number {
+	const pos = getAveragePosition(creeps);
+	const range = creeps.reduce((a, c) => a + c.pos.getRangeTo(pos), 0) / creeps.length;
+	return range;
+}
