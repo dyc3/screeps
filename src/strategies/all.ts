91c493366ca3fd071903755bb615b2ebcfe96c24
Carson McManus <carson.mcmanus1@gmail.com>
2022-05-13 10:38:46 -0400

    move comment

--- src/strategies/all.ts
+++ src/strategies/all.ts
@@ -1,8 +1,8 @@
-// @ts-expect-error TODO: convert to typescript
 import {
 	OffenseStrategyBreakAltPath,
 	OffenseStrategyLureHarrass,
 	OffenseStrategySimpleManual,
+	// @ts-expect-error TODO: convert to typescript
 } from "../strategies/Strategies.js";
 import { OffenseStrategyOvermindRemoteMinerBait } from "../strategies/Overmind";
 
