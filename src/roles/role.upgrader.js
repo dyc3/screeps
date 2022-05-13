dd0cd97af84361beb0dcad82db8f11010e305149
Carson McManus <carson.mcmanus1@gmail.com>
2022-05-13 18:01:25 -0400

    format upgrader

--- src/roles/role.upgrader.js
+++ src/roles/role.upgrader.js
@@ -6,13 +6,14 @@ import toolCreepUpgrader from "../tool.creepupgrader.js";
 
 // get number of upgraders assigned to a room
 function getUpgraderCount(room) {
-	return _.filter(Game.creeps, (creep) => creep.memory.role == "upgrader" && creep.memory.targetRoom == room.name).length;
+	return _.filter(Game.creeps, creep => creep.memory.role == "upgrader" && creep.memory.targetRoom == room.name)
+		.length;
 }
 
 const roleUpgrader = {
 	/** @param {Creep} creep **/
-	findTargetRoom: function(creep) {
-		let rooms = util.getOwnedRooms()
+	findTargetRoom(creep) {
+		let rooms = util.getOwnedRooms();
 		for (let i = 0; i < rooms.length; i++) {
 			let room = rooms[i];
 			if (!room.controller || !room.controller.my) {
@@ -40,7 +41,7 @@ const roleUpgrader = {
 	},
 
 	/** @param {Creep} creep **/
-	run: function(creep) {
+	run(creep) {
 		if (!creep.memory.targetRoom) {
 			creep.memory.targetRoom = this.findTargetRoom(creep);
 			console.log(creep.name, "targetRoom:", creep.memory.targetRoom);
@@ -49,37 +50,36 @@ const roleUpgrader = {
 		if (creep.room.name != creep.memory.targetRoom) {
 			if (Game.rooms[creep.memory.targetRoom]) {
 				creep.travelTo(Game.rooms[creep.memory.targetRoom].controller, { range: 3 });
-			}
-			else {
+			} else {
 				creep.travelTo(new RoomPosition(25, 25, creep.memory.targetRoom), { range: 20 });
 			}
 			return;
 		}
 
-		if(creep.memory.upgrading && creep.carry.energy == 0) {
+		if (creep.memory.upgrading && creep.carry.energy == 0) {
 			creep.memory.upgrading = false;
-			creep.say('gathering');
-		}
-		else if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+			creep.say("gathering");
+		} else if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
 			creep.memory.upgrading = true;
-			creep.say('upgrading');
+			creep.say("upgrading");
 		}
 
-		if(creep.memory.upgrading) {
+		if (creep.memory.upgrading) {
 			if (creep.room.controller.my) {
 				// dont get in the way of the sources while we dont need them
 				let energySources = creep.pos.findInRange(FIND_SOURCES, 1);
 				if (energySources.length > 0) {
 					creep.travelTo(creep.room.controller);
-				}
-				else {
+				} else {
 					if (creep.room.controller.level < 8) {
-						if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+						if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
 							creep.travelTo(creep.room.controller, { range: 3, maxRooms: 1 });
 						}
-					}
-					else {
-						if (creep.room.controller.ticksToDowngrade < 50000 || creep.room.storage.store[RESOURCE_ENERGY] > 500000) {
+					} else {
+						if (
+							creep.room.controller.ticksToDowngrade < 50000 ||
+							(creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] > 500000)
+						) {
 							creep.upgradeController(creep.room.controller);
 						}
 						if (!creep.pos.inRangeTo(creep.room.controller, 3)) {
@@ -90,15 +90,13 @@ const roleUpgrader = {
 						creep.travelTo(creep.room.controller, { maxRooms: 1 });
 					}
 				}
-			}
-			else {
+			} else {
 				creep.travelTo(new RoomPosition(25, 25, creep.memory.targetRoom), { range: 20 });
 			}
-		}
-		else {
+		} else {
 			taskGather.run(creep);
 		}
-	}
+	},
 };
 
 module.exports = roleUpgrader;
