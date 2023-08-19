d7c53707abbf6eb22bebff8956763d4d7ad83ecb
Carson McManus <carson.mcmanus1@gmail.com>
2023-08-19 17:33:11 -0400

    fix lint

--- src/types.ts
+++ src/types.ts
@@ -82,6 +82,7 @@ declare global {
 	}
 
 	// Syntax for adding proprties to `global` (ex "global.log")
+	// eslint-disable-next-line @typescript-eslint/no-namespace
 	namespace NodeJS {
 		interface Global {
 			log: any;
