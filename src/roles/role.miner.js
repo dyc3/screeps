cc4103c81f0c6c2f0c0b00f9d96e047259d50c8d
Carson McManus <carson.mcmanus1@gmail.com>
2023-11-10 12:08:06 -0500

    fix a couple lints

--- src/roles/role.miner.js
+++ src/roles/role.miner.js
@@ -7,15 +7,19 @@ import util from "../util";
  */
 
 const roleMiner = {
-	findMineralTarget: function(creep) {
-		let rooms = util.getOwnedRooms();
-		for (let room of rooms) {
-			let minerals = room.find(FIND_MINERALS);
-			for (let mineral of minerals) {
+	findMineralTarget(creep) {
+		const rooms = util.getOwnedRooms();
+		for (const room of rooms) {
+			const minerals = room.find(FIND_MINERALS);
+			for (const mineral of minerals) {
 				if (util.getStructuresAt(mineral.pos, STRUCTURE_EXTRACTOR).length === 0) {
 					continue;
 				}
-				let minersAssigned = util.getCreeps("miner").filter((creep) => creep.memory.mineralTarget == mineral.id || creep.memory.mineralTargetSecondary == mineral.id);
+				const minersAssigned = util
+					.getCreeps("miner")
+					.filter(
+						c => c.memory.mineralTarget === mineral.id || c.memory.mineralTargetSecondary === mineral.id
+					);
 				if (minersAssigned < 1) {
 					return mineral.id;
 				}
@@ -28,12 +32,23 @@ const roleMiner = {
 	 * Target must be in the same room.
 	 * @returns {Structure} The structure to store the mined minerals in.
 	 **/
-	getTargetStorage: function(creep) {
-		let fillFlags = _.filter(Game.flags, (flag) => { return (flag.name.includes("fill") || flag.name.includes("miner")) && flag.pos.roomName == creep.pos.roomName; });
+	getTargetStorage(creep) {
+		let fillFlags = _.filter(Game.flags, flag => {
+			return (
+				(flag.name.includes("fill") || flag.name.includes("miner")) && flag.pos.roomName == creep.pos.roomName
+			);
+		});
 		for (let flag of fillFlags) {
 			let material = flag.name.split(":")[1];
 			if (creep.carry[material] > 0) {
-				let target = flag.pos.lookFor(LOOK_STRUCTURES).filter(struct =>  struct.structureType == STRUCTURE_LAB || struct.structureType == STRUCTURE_TERMINAL || struct.structureType == STRUCTURE_FACTORY )[0];
+				let target = flag.pos
+					.lookFor(LOOK_STRUCTURES)
+					.filter(
+						struct =>
+							struct.structureType == STRUCTURE_LAB ||
+							struct.structureType == STRUCTURE_TERMINAL ||
+							struct.structureType == STRUCTURE_FACTORY
+					)[0];
 				creep.memory.storageTarget = target.id;
 				creep.memory.materialToStore = material;
 				return target;
@@ -42,15 +57,14 @@ const roleMiner = {
 
 		if (!creep.memory.useSecondaryRoute) {
 			creep.memory.storageTarget = creep.room.storage.id;
-		}
-		else {
+		} else {
 			creep.memory.storageTargetSecondary = creep.room.storage.id;
 		}
 		delete creep.memory.materialToStore;
 		return creep.room.storage;
 	},
 
-	run: function(creep) {
+	run(creep) {
 		if (!creep.memory.mineralTarget) {
 			if (Game.cpu.bucket <= 100) {
 				return;
@@ -81,12 +95,24 @@ const roleMiner = {
 		}
 
 		// HACK: If a mineral can be mined, then ticksToRegeneration is actually undefined instead of 0
-		if (mineralTargetSecondary && !creep.memory.useSecondaryRoute && mineralTarget.ticksToRegeneration > 0 && (mineralTargetSecondary.ticksToRegeneration === undefined || mineralTarget.ticksToRegeneration > mineralTargetSecondary.ticksToRegeneration) && total_carry == 0) {
+		if (
+			mineralTargetSecondary &&
+			!creep.memory.useSecondaryRoute &&
+			mineralTarget.ticksToRegeneration > 0 &&
+			(mineralTargetSecondary.ticksToRegeneration === undefined ||
+				mineralTarget.ticksToRegeneration > mineralTargetSecondary.ticksToRegeneration) &&
+			total_carry == 0
+		) {
 			creep.memory.useSecondaryRoute = true;
 			creep.say("switch->2");
 			return;
-		}
-		else if (creep.memory.useSecondaryRoute && mineralTargetSecondary.ticksToRegeneration > 0 && (mineralTarget.ticksToRegeneration === undefined || mineralTargetSecondary.ticksToRegeneration > mineralTarget.ticksToRegeneration) && total_carry == 0) {
+		} else if (
+			creep.memory.useSecondaryRoute &&
+			mineralTargetSecondary.ticksToRegeneration > 0 &&
+			(mineralTarget.ticksToRegeneration === undefined ||
+				mineralTargetSecondary.ticksToRegeneration > mineralTarget.ticksToRegeneration) &&
+			total_carry == 0
+		) {
 			creep.memory.useSecondaryRoute = false;
 			creep.say("switch->1");
 			return;
@@ -98,7 +124,14 @@ const roleMiner = {
 			delete creep.memory.storageTarget;
 			delete creep.memory.materialToStore;
 		}
-		if (creep.memory.mining && (total_carry == creep.carryCapacity || ((creep.memory.useSecondaryRoute ? mineralTargetSecondary.ticksToRegeneration : mineralTarget.ticksToRegeneration) > 0 && total_carry > 0))) {
+		if (
+			creep.memory.mining &&
+			(total_carry == creep.carryCapacity ||
+				((creep.memory.useSecondaryRoute
+					? mineralTargetSecondary.ticksToRegeneration
+					: mineralTarget.ticksToRegeneration) > 0 &&
+					total_carry > 0))
+		) {
 			creep.memory.mining = false;
 			creep.say("storing");
 		}
@@ -110,41 +143,42 @@ const roleMiner = {
 					creep.travelTo(creep.memory.useSecondaryRoute ? mineralTargetSecondary : mineralTarget);
 					break;
 				default:
-
 			}
-		}
-		else {
+		} else {
 			let storageTarget = null;
-			if ((!creep.memory.useSecondaryRoute && !creep.memory.storageTarget) || (creep.memory.useSecondaryRoute && !creep.memory.storageTargetSecondary)) {
+			if (
+				(!creep.memory.useSecondaryRoute && !creep.memory.storageTarget) ||
+				(creep.memory.useSecondaryRoute && !creep.memory.storageTargetSecondary)
+			) {
 				storageTarget = this.getTargetStorage(creep);
-			}
-			else {
+			} else {
 				if (!creep.memory.useSecondaryRoute) {
 					storageTarget = Game.getObjectById(creep.memory.storageTarget);
-				}
-				else {
+				} else {
 					storageTarget = Game.getObjectById(creep.memory.storageTargetSecondary);
 				}
 			}
 
 			if (creep.pos.isNearTo(storageTarget)) {
 				if (creep.memory.materialToStore) {
-					creep.transfer(storageTarget, creep.memory.materialToStore, creep.carry[creep.memory.materialToStore]);
-				}
-				else {
+					creep.transfer(
+						storageTarget,
+						creep.memory.materialToStore,
+						creep.carry[creep.memory.materialToStore]
+					);
+				} else {
 					for (let resource of RESOURCES_ALL) {
 						if (creep.carry[resource] > 0) {
 							creep.transfer(storageTarget, resource);
 						}
 					}
 				}
-			}
-			else {
+			} else {
 				creep.travelTo(storageTarget);
 			}
 		}
-	}
-}
+	},
+};
 
 module.exports = roleMiner;
 export default roleMiner;
