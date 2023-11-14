9873807afba37a2bc32d69acf27b04a50a851797
Carson McManus <carson.mcmanus1@gmail.com>
2023-11-14 15:20:15 -0500

    adjust eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,6 +52,7 @@ module.exports = {
     "@typescript-eslint/prefer-for-of": "error",
     "@typescript-eslint/space-within-parens": ["off", "never"],
     "@typescript-eslint/unified-signatures": "error",
+    "@typescript-eslint/restrict-template-expressions": "off",
     "arrow-parens": ["off", "as-needed"],
     camelcase: "error",
     complexity: "off",
