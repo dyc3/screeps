75ece8c01eda1778030470debdab4e8550849f4d
Carson McManus <carson.mcmanus1@gmail.com>
2023-11-14 09:22:43 -0500

    convert tool.friends to ts

---
+++ src/tool.friends.ts
@@ -0,0 +1,29 @@
+const toolFriends = {
+	friends: ["Segmentation_Fault", "Azrules", "ragingblizzard", "choudama", "KennsworthS", "Mototroller"],
+
+	/**
+	 * @deprecated Use isFriendly instead
+	 * @returns
+	 */
+	isCreepFriendly(creep: AnyCreep): boolean {
+		return this.isFriendly(struct);
+	},
+
+	/**
+	 * @deprecated Use isFriendly instead
+	 * @returns
+	 */
+	isStructureFriendly(struct: OwnedStructure): boolean {
+		return this.isFriendly(struct);
+	},
+
+	isFriendly(obj: OwnedStructure | AnyCreep): boolean {
+		if (obj.owner) {
+			return obj.my || this.friends.includes(obj.owner.username);
+		}
+		return true;
+	},
+};
+
+module.exports = toolFriends;
+export default toolFriends;
