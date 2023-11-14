72d900094e4296dc782bb49ee30ef742a400350b
Carson McManus <carson.mcmanus1@gmail.com>
2023-11-14 08:21:34 -0500

    fix some lints

--- src/tool.energysource.ts
+++ src/tool.energysource.ts
@@ -1,9 +1,9 @@
-import util from "./util";
 import { Role } from "./roles/meta";
+import util from "./util";
 
 const toolEnergySource = {
 	// adjusted: adjust harvester count for if containers/storage/links are nearby
-	getMaxHarvesters(energySource: Source, adjusted = false) {
+	getMaxHarvesters(energySource: Source, adjusted = false): number {
 		if (util.getOwnedRooms().length > 1) {
 			return 1;
 		}
@@ -22,9 +22,9 @@ const toolEnergySource = {
 			if (
 				energySource.pos.findInRange(FIND_STRUCTURES, 2, {
 					filter: struct =>
-						struct.structureType == STRUCTURE_CONTAINER ||
-						struct.structureType == STRUCTURE_LINK ||
-						struct.structureType == STRUCTURE_STORAGE,
+						struct.structureType === STRUCTURE_CONTAINER ||
+						struct.structureType === STRUCTURE_LINK ||
+						struct.structureType === STRUCTURE_STORAGE,
 				}).length > 0
 			) {
 				count = 1;
@@ -33,24 +33,24 @@ const toolEnergySource = {
 		return count > 2 ? 2 : count;
 	},
 
-	getHarvesters(energySource: Source) {
+	getHarvesters(energySource: Source): number {
 		let count = 0;
 		const harvesters = util.getCreeps(Role.Harvester);
 		for (const harvester of harvesters) {
-			if (harvester.memory.harvestTarget == energySource.id) {
+			if (harvester.memory.harvestTarget === energySource.id) {
 				count++;
 			}
 		}
 		return count;
 	},
 
-	canAssignSource(energySource: Source) {
+	canAssignSource(energySource: Source): boolean {
 		const maxHarvestersOnSource = this.getMaxHarvesters(energySource, true);
 		const harvestersOnSource = this.getHarvesters(energySource);
 		return harvestersOnSource < maxHarvestersOnSource;
 	},
 
-	getHarvesterQuota(room: Room) {
+	getHarvesterQuota(this: void, room: Room): number {
 		if (!room.controller) {
 			return 0;
 		}
@@ -64,18 +64,17 @@ const toolEnergySource = {
 		}
 	},
 
-	drawAssignedCounts() {
+	drawAssignedCounts(): void {
 		const rooms = util.getOwnedRooms();
-		for (let r = 0; r < rooms.length; r++) {
-			const room = rooms[r];
+		for (const room of rooms) {
 			const sources = room.find(FIND_SOURCES);
-			for (let i = 0; i < sources.length; i++) {
-				const count = this.getHarvesters(sources[i]);
-				const max = this.getMaxHarvesters(sources[i], true);
+			for (const source of sources) {
+				const count = this.getHarvesters(source);
+				const max = this.getMaxHarvesters(source, true);
 
-				const text = count + "/" + max;
+				const text = `${count}/${max}`;
 				const color = count <= max ? "#11dd11" : "#dd1111";
-				room.visual.text(text, sources[i].pos, { color, font: 0.4, stroke: "#000" });
+				room.visual.text(text, source.pos, { color, font: 0.4, stroke: "#000" });
 			}
 		}
 	},
