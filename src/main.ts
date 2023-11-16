e2d1cbc182f8cfd23000ffebe5c4ca5547c931cd
Carson McManus <carson.mcmanus1@gmail.com>
2023-11-16 17:32:06 -0500

    add shit to profiler

--- src/main.ts
+++ src/main.ts
@@ -1995,6 +1995,9 @@ profiler.registerObject(roleRemoteHarvester, "role.remoteharvester");
 profiler.registerObject(roleCarrier, "role.carrier");
 profiler.registerObject(visualize, "visualize");
 profiler.registerObject(util, "util");
+profiler.registerObject(brainGuard, "brain.guard");
+profiler.registerClass(ObserveQueue, "ObserveQueue");
+profiler.registerClass(JobRunner, "JobRunner");
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
