5f4d6fa0034700306049c1f7b3d4c07810f7e7d0
Carson McManus <carson.mcmanus1@gmail.com>
2023-11-17 17:34:20 -0500

    carrier: convert to ts

--- src/roles/role.carrier.ts
+++ src/roles/role.carrier.ts
@@ -1,68 +1,70 @@
 import * as cartographer from "screeps-cartographer";
 
+import { Role } from "./meta";
 import util from "../util";
 
 // Game.spawns["Spawn5"].createCreep([WORK,WORK,WORK,WORK,MOVE,MOVE,MOVE,MOVE], "remoteharvester_1", {role:"remoteharvester", keepAlive:true, stage: 0 }); Game.spawns["Spawn1"].createCreep([CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], "carrier_1", {role:"carrier", keepAlive:true, stage: 0 })
 
-let roleCarrier = {
-	findDespositTarget(creep) {
-		if (creep.memory.mode === "remote-mining" && !creep.memory.harvestTarget) {
-			creep.log("Can't find despositTarget without harvestTarget");
-			return;
-		}
+function findDespositTarget(creep: Creep) {
+	if (creep.memory.mode === "remote-mining" && !creep.memory.harvestTarget) {
+		creep.log("Can't find despositTarget without harvestTarget");
+		return;
+	}
 
-		let pos = creep.pos;
-		if (creep.memory.mode === "remote-mining") {
-			pos = new RoomPosition(
-				creep.memory.harvestTarget.x,
-				creep.memory.harvestTarget.y,
-				creep.memory.harvestTarget.roomName
-			);
-		} else if (creep.memory.mode === "invader-core-harvesting") {
-			pos = new RoomPosition(25, 25, creep.memory.targetRoom);
-		}
+	let pos = creep.pos;
+	if (creep.memory.mode === "remote-mining") {
+		pos = new RoomPosition(
+			creep.memory.harvestTarget.x,
+			creep.memory.harvestTarget.y,
+			creep.memory.harvestTarget.roomName
+		);
+	} else if (creep.memory.mode === "invader-core-harvesting") {
+		pos = new RoomPosition(25, 25, creep.memory.targetRoom);
+	}
 
-		let rooms = _.filter(util.findClosestOwnedRooms(pos), r => r.storage);
-		if (rooms.length === 0) {
-			creep.log("ERR: All rooms don't have storage");
-			return;
-		}
+	const rooms = _.filter(util.findClosestOwnedRooms(pos), r => r.storage);
+	if (rooms.length === 0) {
+		creep.log("ERR: All rooms don't have storage");
+		return;
+	}
 
-		return rooms[0].storage.id;
-	},
+	return rooms[0].storage.id;
+}
 
-	passiveMaintainRoads(creep) {
-		if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
-			return;
-		}
+function passiveMaintainRoads(creep: Creep) {
+	if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
+		return;
+	}
 
-		let construction = creep.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 3);
-		if (construction.length > 0) {
-			creep.build(construction[0]);
-			return;
-		}
+	const construction = creep.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 3);
+	if (construction.length > 0) {
+		creep.build(construction[0]);
+		return;
+	}
 
-		let structs = creep.pos.findInRange(FIND_STRUCTURES, 3).filter(s => s.hits < s.hitsMax);
-		if (structs.length > 0) {
-			structs = _.sortByOrder(
-				structs,
-				[s => s.structureType === STRUCTURE_RAMPART, s => s.hits / s.hitsMax],
-				["desc", "asc"]
-			);
-			creep.repair(structs[0]);
-			return;
-		}
+	let structs = creep.pos.findInRange(FIND_STRUCTURES, 3).filter(s => s.hits < s.hitsMax);
+	if (structs.length > 0) {
+		structs = _.sortByOrder(
+			structs,
+			[s => s.structureType === STRUCTURE_RAMPART, s => s.hits / s.hitsMax],
+			["desc", "asc"]
+		);
+		creep.repair(structs[0]);
+		return;
+	}
 
-		// FIXME: This gets really messy, we should plan the roads ahead of time.
-		// 		if (creep.memory.depositTarget && creep.room.name !== Game.getObjectById(creep.memory.depositTarget).room.name) {
-		// 			creep.pos.createConstructionSite(STRUCTURE_ROAD);
-		// 		}
-	},
+	// FIXME: This gets really messy, we should plan the roads ahead of time.
+	// 		if (creep.memory.depositTarget && creep.room.name !== Game.getObjectById(creep.memory.depositTarget).room.name) {
+	// 			creep.pos.createConstructionSite(STRUCTURE_ROAD);
+	// 		}
+}
 
+const roleCarrier = {
 	modes: {
-		"remote-mining"(roleCarrier, creep) {
+		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+		"remote-mining": (creep: Creep) => {
 			if (!creep.memory.depositTarget) {
-				creep.memory.depositTarget = roleCarrier.findDespositTarget(creep);
+				creep.memory.depositTarget = findDespositTarget(creep);
 			}
 
 			if (!creep.memory.harvestTarget) {
@@ -77,24 +79,36 @@ let roleCarrier = {
 				return;
 			}
 
-			let harvestTarget = _.find(
+			const harvestTarget = _.find(
 				Memory.remoteMining.targets,
-				target => target.id === creep.memory.harvestTarget.id
+				target => target.id === creep.memory.harvestTarget
 			);
 
 			if (!creep.memory.delivering && creep.room.name !== harvestTarget.roomName && harvestTarget.danger === 0) {
 				cartographer.moveTo(creep, new RoomPosition(harvestTarget.x, harvestTarget.y, harvestTarget.roomName));
 				return;
 			} else if (!creep.memory.delivering && harvestTarget.danger > 0) {
-				let dangerPos = new RoomPosition(
+				const dangerPos = new RoomPosition(
 					harvestTarget.dangerPos[harvestTarget.danger].x,
 					harvestTarget.dangerPos[harvestTarget.danger].y,
 					harvestTarget.dangerPos[harvestTarget.danger].roomName
 				);
-				cartographer.moveTo(creep, dangerPos, { range: 1 });
+				cartographer.moveTo(creep, dangerPos, {
+					avoidTargets(roomName: string) {
+						const room = Game.rooms[roomName];
+						if (!room) return [];
+						return room.find(FIND_HOSTILE_CREEPS).map(c => {
+							return {
+								pos: c.pos,
+								range: 5,
+							};
+						});
+					},
+					avoidTargetGradient: 0.9,
+				});
 				return;
 			}
-			let harvestPos = new RoomPosition(
+			const harvestPos = new RoomPosition(
 				harvestTarget.harvestPos.x,
 				harvestTarget.harvestPos.y,
 				harvestTarget.roomName
@@ -102,7 +116,7 @@ let roleCarrier = {
 
 			if (!creep.memory.droppedEnergyId) {
 				try {
-					let lookResult = harvestPos.lookFor(LOOK_RESOURCES);
+					const lookResult = harvestPos.lookFor(LOOK_RESOURCES);
 					if (lookResult.length > 0) {
 						creep.memory.droppedEnergyId = lookResult[0].id;
 					}
@@ -111,18 +125,22 @@ let roleCarrier = {
 				}
 			}
 
-			if (creep.memory.delivering && _.sum(creep.store) === 0) {
+			if (creep.memory.delivering && creep.store.getUsedCapacity() === 0) {
 				creep.memory.delivering = false;
-			} else if (!creep.memory.delivering && _.sum(creep.store) === creep.store.getCapacity()) {
+			} else if (!creep.memory.delivering && creep.store.getFreeCapacity() === 0) {
 				creep.memory.delivering = true;
 			}
 
 			if (creep.memory.delivering) {
-				let depositTarget = Game.getObjectById(creep.memory.depositTarget);
+				const depositTarget = Game.getObjectById(creep.memory.depositTarget);
+				if (!depositTarget) {
+					creep.log("ERR: can't find deposit target");
+					return;
+				}
 				if (creep.pos.isNearTo(depositTarget)) {
 					creep.transfer(depositTarget, RESOURCE_ENERGY);
 				} else {
-					let obstacles = util.getCreeps("harvester", "relay");
+					const obstacles = util.getCreeps(Role.Harvester, Role.Relay);
 					cartographer.moveTo(creep, depositTarget, {
 						obstacles,
 						ensurePath: true,
@@ -139,7 +157,7 @@ let roleCarrier = {
 					if (creep.store[RESOURCE_ENERGY] > 0) {
 						creep.memory.delivering = true;
 					} else {
-						let dangerPos = new RoomPosition(
+						const dangerPos = new RoomPosition(
 							harvestTarget.dangerPos[harvestTarget.danger].x,
 							harvestTarget.dangerPos[harvestTarget.danger].y,
 							harvestTarget.dangerPos[harvestTarget.danger].roomName
@@ -151,19 +169,19 @@ let roleCarrier = {
 
 				if (util.isTreasureRoom(harvestTarget.roomName)) {
 					if (creep.pos.isNearTo(harvestPos)) {
-						let tombstones = harvestPos.findInRange(FIND_TOMBSTONES, 2);
+						const tombstones = harvestPos.findInRange(FIND_TOMBSTONES, 2);
 						if (tombstones.length > 0 && tombstones[0].store[RESOURCE_ENERGY] > 0) {
 							creep.withdraw(tombstones[0], RESOURCE_ENERGY);
 							return;
 						}
 					}
 				}
-				let dropped = Game.getObjectById(creep.memory.droppedEnergyId);
+				const dropped = Game.getObjectById(creep.memory.droppedEnergyId);
 				if (!dropped) {
 					delete creep.memory.droppedEnergyId;
 				}
 				if (!creep.pos.isNearTo(harvestPos)) {
-					let obstacles = util.getCreeps("harvester", "relay");
+					const obstacles = util.getCreeps("harvester", "relay");
 					cartographer.moveTo(creep, harvestPos, { obstacles });
 					// cartographer.moveTo(creep, harvestPos);
 				} else if (dropped) {
@@ -172,17 +190,18 @@ let roleCarrier = {
 			}
 
 			if (creep.getActiveBodyparts(WORK) > 0) {
-				roleCarrier.passiveMaintainRoads(creep);
+				passiveMaintainRoads(creep);
 			}
 		},
-		"invader-core-harvesting"(roleCarrier, creep) {
+		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+		"invader-core-harvesting"(creep: Creep) {
 			if (!creep.memory.targetRoom) {
 				creep.log("needs targetRoom to be set externally");
 				return;
 			}
 
 			if (!creep.memory.depositTarget) {
-				creep.memory.depositTarget = roleCarrier.findDespositTarget(creep);
+				creep.memory.depositTarget = findDespositTarget(creep);
 			}
 
 			if (creep.memory.delivering && _.sum(creep.store) === 0) {
@@ -193,9 +212,9 @@ let roleCarrier = {
 
 			if (creep.memory.delivering) {
 				// take resources back to deposit target
-				let depositTarget = Game.getObjectById(creep.memory.depositTarget);
+				const depositTarget = Game.getObjectById(creep.memory.depositTarget);
 				if (creep.pos.isNearTo(depositTarget)) {
-					for (let resource of RESOURCES_ALL) {
+					for (const resource of RESOURCES_ALL) {
 						if (creep.store[resource] > 0) {
 							creep.transfer(depositTarget, resource);
 						}
@@ -214,14 +233,14 @@ let roleCarrier = {
 					.concat(util.getStructures(creep.room, STRUCTURE_CONTAINER));
 				containers = _.filter(containers, s => _.sum(s.store) > 0);
 				if (containers.length > 0) {
-					let target = containers[0];
+					const target = containers[0];
 					creep.log("target: ", target);
 					if (creep.pos.isNearTo(target)) {
 						for (const resource of RESOURCES_ALL) {
 							if (target.store[resource] === 0) {
 								continue;
 							}
-							let withdrawResult = creep.withdraw(target, resource);
+							const withdrawResult = creep.withdraw(target, resource);
 							if (withdrawResult !== OK) {
 								creep.log("ERR: can't withdraw from target", target, "error", withdrawResult);
 								break;
@@ -238,8 +257,7 @@ let roleCarrier = {
 		},
 	},
 
-	/** @param {Creep} creep **/
-	run(creep) {
+	run(creep: Creep): void {
 		if (!creep.memory.mode) {
 			creep.memory.mode = "remote-mining";
 		}
