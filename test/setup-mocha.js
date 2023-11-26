c188fa4731f8000f11aa805d3115f9b94c96f769
Carson McManus <carson.mcmanus1@gmail.com>
2023-11-26 12:57:04 -0500

    tests: adjust globals so stuff gets imported correctly

--- test/setup-mocha.js
+++ test/setup-mocha.js
@@ -1,9 +1,62 @@
-//inject mocha globally to allow custom interface refer without direct import - bypass bundle issue
-global._ = require('lodash');
-global.mocha = require('mocha');
-global.chai = require('chai');
-global.sinon = require('sinon');
-global.chai.use(require('sinon-chai'));
+// inject mocha globally to allow custom interface refer without direct import - bypass bundle issue
+global._ = require("lodash");
+global.mocha = require("mocha");
+global.chai = require("chai");
+global.sinon = require("sinon");
+global.chai.use(require("sinon-chai"));
+
+global.Game = {
+	time: 12345,
+};
+global.Memory = {};
+global.Room = {
+	prototype: {},
+};
+global.Structure = {};
+global.Creep = {
+	prototype: {},
+};
+global.PowerCreep = {
+	prototype: {},
+};
+global.Flag = {};
+global.RawMemory = {};
+global.Spawn = {};
+global.Source = {};
+global.Tombstone = {};
+global.RoomPosition = {};
+global.STRUCTURE_SPAWN = "spawn";
+global.STRUCTURE_EXTENSION = "extension";
+global.STRUCTURE_ROAD = "road";
+global.STRUCTURE_WALL = "constructedWall";
+global.STRUCTURE_RAMPART = "rampart";
+global.STRUCTURE_KEEPER_LAIR = "keeperLair";
+global.STRUCTURE_PORTAL = "portal";
+global.STRUCTURE_CONTROLLER = "controller";
+global.STRUCTURE_LINK = "link";
+global.STRUCTURE_STORAGE = "storage";
+global.STRUCTURE_TOWER = "tower";
+global.STRUCTURE_OBSERVER = "observer";
+global.STRUCTURE_POWER_BANK = "powerBank";
+global.STRUCTURE_POWER_SPAWN = "powerSpawn";
+global.STRUCTURE_EXTRACTOR = "extractor";
+global.STRUCTURE_LAB = "lab";
+global.STRUCTURE_TERMINAL = "terminal";
+global.STRUCTURE_CONTAINER = "container";
+global.STRUCTURE_NUKER = "nuker";
+global.STRUCTURE_FACTORY = "factory";
+global.STRUCTURE_INVADER_CORE = "invaderCore";
+global.RESOURCE_ENERGY = "energy";
+global.RESOURCE_POWER = "power";
+
+global.WORK = "work";
+global.CARRY = "carry";
+global.MOVE = "move";
+global.ATTACK = "attack";
+global.RANGED_ATTACK = "ranged_attack";
+global.TOUGH = "tough";
+global.HEAL = "heal";
+global.CLAIM = "claim";
 
 // Override ts-node compiler options
-process.env.TS_NODE_PROJECT = 'tsconfig.test.json'
+process.env.TS_NODE_PROJECT = "tsconfig.test.json";
