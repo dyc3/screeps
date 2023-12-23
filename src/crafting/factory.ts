bcc2af8d6e9e980539dba33c27ac1d6a1ed1d1f6
Carson McManus <carson.mcmanus1@gmail.com>
2023-12-23 09:37:12 -0500

    work-factories: move job code into a new file

---
+++ src/crafting/factory.ts
@@ -0,0 +1,101 @@
+import { JobRunner } from "jobs";
+import { util } from "../util";
+
+const JOB_NAME = "work-factories";
+
+function doWorkFactories() {
+	let minWaitTime = JobRunner.getInstance().getInterval(JOB_NAME) ?? Infinity; // minimum time to wait before running again
+	const rooms = util.getOwnedRooms();
+	for (const room of rooms) {
+		const factory = util.getStructures(room, STRUCTURE_FACTORY)[0];
+
+		if (!factory || factory.cooldown > 0) {
+			continue;
+		}
+
+		// FIXME: make this more dynamic
+		// right now, everything is hard coded
+
+		const productionTargets: (CommodityConstant | RESOURCE_ENERGY)[] = [
+			RESOURCE_UTRIUM_BAR,
+			RESOURCE_ZYNTHIUM_BAR,
+			RESOURCE_LEMERGIUM_BAR,
+			RESOURCE_KEANIUM_BAR,
+			RESOURCE_REDUCTANT,
+			RESOURCE_OXIDANT,
+			RESOURCE_PURIFIER,
+			RESOURCE_GHODIUM_MELT,
+
+			RESOURCE_ALLOY,
+
+			RESOURCE_CELL,
+
+			RESOURCE_WIRE,
+
+			RESOURCE_ESSENCE,
+			RESOURCE_EMANATION,
+			RESOURCE_SPIRIT,
+			RESOURCE_EXTRACT,
+			RESOURCE_CONCENTRATE,
+		];
+
+		if (room.storage) {
+			if (room.storage.store[RESOURCE_ENERGY] > 800000 && factory.store[RESOURCE_BATTERY] < 10000) {
+				productionTargets.push(RESOURCE_BATTERY);
+			} else if (room.storage.store[RESOURCE_ENERGY] < 600000) {
+				productionTargets.push(RESOURCE_ENERGY);
+			}
+		}
+
+		for (const productionTarget of productionTargets) {
+			console.log(`[work-factories] production target: ${productionTarget}`);
+			let canProduce = true;
+			const commodity = COMMODITIES[productionTarget];
+			const neededLevel = commodity.level ?? 0;
+			if (factory.level && neededLevel > factory.level) {
+				console.log(`[work-factories] factory is level ${factory.level}, but level ${neededLevel} is required`);
+				canProduce = false;
+				continue;
+			}
+
+			for (const _component in commodity.components) {
+				const component = _component as CommodityConstant;
+				// console.log(`[work-factories] factory has component ${component}?`);
+				if (factory.store.getUsedCapacity(component) === 0) {
+					console.log(`[work-factories] no ${component} found`);
+					canProduce = false;
+					break;
+				}
+				// console.log(`[work-factories] found ${factory.store[component]} ${component}`);
+				if (factory.store.getUsedCapacity(component) < commodity.components[component]) {
+					console.log(
+						`[work-factories] not enough ${component}, currently have ${factory.store[component]}/${commodity.components[component]}`
+					);
+					canProduce = false;
+					break;
+				}
+			}
+
+			console.log(`[work-factories] production target: ${productionTarget}, can produce: ${canProduce}`);
+			if (canProduce) {
+				const productionResult = factory.produce(productionTarget);
+				if (productionResult !== OK) {
+					console.log(`[work-factories] unable to produce ${productionTarget} => ${productionResult}`);
+				} else {
+					minWaitTime = Math.min(minWaitTime, 20); // FIXME: this is hardcoded, get real cooldown from factory
+					break;
+				}
+			}
+		}
+	}
+
+	JobRunner.getInstance().forceRunAfter(JOB_NAME, minWaitTime);
+}
+
+export function registerJob(): void {
+	JobRunner.getInstance().registerJob({
+		name: JOB_NAME,
+		run: doWorkFactories,
+		interval: 20,
+	});
+}
--- src/main.ts
+++ src/main.ts
@@ -97,6 +97,7 @@ import { Scout } from "roles/role.scout";
 import util from "./util";
 import visualize from "./visualize";
 import { Role } from "./roles/meta";
+import { registerJob as registerJobFactory } from "./crafting/factory";
 
 import roleHarvester from "roles/role.harvester";
 import roleUpgrader from "roles/role.upgrader";
@@ -1299,95 +1300,6 @@ function satisfyClaimTargets() {
 	}
 }
 
-function doWorkFactories() {
-	let minWaitTime = runner.getInterval("work-factories") ?? Infinity; // minimum time to wait before running again
-	const rooms = util.getOwnedRooms();
-	for (const room of rooms) {
-		const factory = util.getStructures(room, STRUCTURE_FACTORY)[0];
-
-		if (!factory || factory.cooldown > 0) {
-			continue;
-		}
-
-		// FIXME: make this more dynamic
-		// right now, everything is hard coded
-
-		const productionTargets: (CommodityConstant | RESOURCE_ENERGY)[] = [
-			RESOURCE_UTRIUM_BAR,
-			RESOURCE_ZYNTHIUM_BAR,
-			RESOURCE_LEMERGIUM_BAR,
-			RESOURCE_KEANIUM_BAR,
-			RESOURCE_REDUCTANT,
-			RESOURCE_OXIDANT,
-			RESOURCE_PURIFIER,
-			RESOURCE_GHODIUM_MELT,
-
-			RESOURCE_ALLOY,
-
-			RESOURCE_CELL,
-
-			RESOURCE_WIRE,
-
-			RESOURCE_ESSENCE,
-			RESOURCE_EMANATION,
-			RESOURCE_SPIRIT,
-			RESOURCE_EXTRACT,
-			RESOURCE_CONCENTRATE,
-		];
-
-		if (room.storage) {
-			if (room.storage.store[RESOURCE_ENERGY] > 800000 && factory.store[RESOURCE_BATTERY] < 10000) {
-				productionTargets.push(RESOURCE_BATTERY);
-			} else if (room.storage.store[RESOURCE_ENERGY] < 600000) {
-				productionTargets.push(RESOURCE_ENERGY);
-			}
-		}
-
-		for (const productionTarget of productionTargets) {
-			console.log(`[work-factories] production target: ${productionTarget}`);
-			let canProduce = true;
-			const commodity = COMMODITIES[productionTarget];
-			const neededLevel = commodity.level ?? 0;
-			if (factory.level && neededLevel > factory.level) {
-				console.log(`[work-factories] factory is level ${factory.level}, but level ${neededLevel} is required`);
-				canProduce = false;
-				break;
-			}
-
-			for (const _component in commodity.components) {
-				const component = _component as CommodityConstant;
-				// console.log(`[work-factories] factory has component ${component}?`);
-				if (factory.store.getUsedCapacity(component) === 0) {
-					console.log(`[work-factories] no ${component} found`);
-					canProduce = false;
-					break;
-				}
-				// console.log(`[work-factories] found ${factory.store[component]} ${component}`);
-				if (factory.store.getUsedCapacity(component) < commodity.components[component]) {
-					console.log(
-						`[work-factories] not enough ${component}, currently have ${factory.store[component]}/${commodity.components[component]}`
-					);
-					canProduce = false;
-					break;
-				}
-			}
-
-			console.log(`[work-factories] production target: ${productionTarget}, can produce: ${canProduce}`);
-			if (canProduce) {
-				const productionResult = factory.produce(productionTarget);
-				if (productionResult !== OK) {
-					console.log(`[work-factories] unable to produce ${productionTarget} => ${productionResult}`);
-				} else {
-					minWaitTime = Math.min(minWaitTime, 20); // FIXME: this is hardcoded, get real cooldown from factory
-					break;
-				}
-			}
-		}
-	}
-
-	runner.forceRunAfter("work-factories", minWaitTime);
-}
-
 runner.registerJob({
 	name: "creep-spawning",
 	run: doCreepSpawning,
@@ -1438,11 +1350,7 @@ runner.registerJob({
 	run: satisfyClaimTargets,
 	interval: 50,
 });
-runner.registerJob({
-	name: "work-factories",
-	run: doWorkFactories,
-	interval: 20,
-});
+registerJobFactory();
 runner.registerJob({
 	name: "op-guard",
 	run() {
