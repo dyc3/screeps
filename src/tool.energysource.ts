447351d804b9e1b6e9f92b22ff72ddbfa69b9551
Carson McManus <carson.mcmanus1@gmail.com>
2023-12-30 09:31:35 -0500

    hack: force max harvesters per source to be 1

--- src/tool.energysource.ts
+++ src/tool.energysource.ts
@@ -4,34 +4,35 @@ import util from "./util";
 const toolEnergySource = {
 	// adjusted: adjust harvester count for if containers/storage/links are nearby
 	getMaxHarvesters(energySource: Source, adjusted = false): number {
-		if (util.getOwnedRooms().length > 1) {
-			return 1;
-		}
-		const terrain = Game.map.getRoomTerrain(energySource.pos.roomName);
-		let count = 0;
-		for (let y = energySource.pos.y - 1; y <= energySource.pos.y + 1; y++) {
-			for (let x = energySource.pos.x - 1; x <= energySource.pos.x + 1; x++) {
-				const thisterrain = terrain.get(x, y);
-				// eslint-disable-next-line no-bitwise
-				if ((thisterrain & (TERRAIN_MASK_WALL | TERRAIN_MASK_LAVA)) > 0) {
-					continue;
-				}
-				count++;
-			}
-		}
-		if (adjusted) {
-			if (
-				energySource.pos.findInRange(FIND_STRUCTURES, 2, {
-					filter: struct =>
-						struct.structureType === STRUCTURE_CONTAINER ||
-						struct.structureType === STRUCTURE_LINK ||
-						struct.structureType === STRUCTURE_STORAGE,
-				}).length > 0
-			) {
-				count = 1;
-			}
-		}
-		return count > 2 ? 2 : count;
+		return 1;
+		// if (util.getOwnedRooms().length > 1) {
+		// 	return 1;
+		// }
+		// const terrain = Game.map.getRoomTerrain(energySource.pos.roomName);
+		// let count = 0;
+		// for (let y = energySource.pos.y - 1; y <= energySource.pos.y + 1; y++) {
+		// 	for (let x = energySource.pos.x - 1; x <= energySource.pos.x + 1; x++) {
+		// 		const thisterrain = terrain.get(x, y);
+		// 		// eslint-disable-next-line no-bitwise
+		// 		if ((thisterrain & (TERRAIN_MASK_WALL | TERRAIN_MASK_LAVA)) > 0) {
+		// 			continue;
+		// 		}
+		// 		count++;
+		// 	}
+		// }
+		// if (adjusted) {
+		// 	if (
+		// 		energySource.pos.findInRange(FIND_STRUCTURES, 2, {
+		// 			filter: struct =>
+		// 				struct.structureType === STRUCTURE_CONTAINER ||
+		// 				struct.structureType === STRUCTURE_LINK ||
+		// 				struct.structureType === STRUCTURE_STORAGE,
+		// 		}).length > 0
+		// 	) {
+		// 		count = 1;
+		// 	}
+		// }
+		// return count > 2 ? 2 : count;
 	},
 
 	getHarvesters(energySource: Source): number {
