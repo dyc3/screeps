48766513c8fc73c3f0c112e10a4e28f509697b32
Carson McManus <carson.mcmanus1@gmail.com>
2024-01-02 14:10:33 -0500

    remotemining: don't allocate creeps for targets that can't be mined yet

--- src/remotemining.ts
+++ src/remotemining.ts
@@ -82,6 +82,11 @@ function allocateCreeps(): AllocateResult {
 		if (target.paused) {
 			continue;
 		}
+		const room = Game.rooms[target.roomName];
+		if (room && room.controller!.reservation && room.controller!.reservation.username !== global.WHOAMI) {
+			// don't allocate creeps to rooms that are currently not reserved by us
+			continue;
+		}
 		// HACK: idk whybut for some reason creepCarriers is sometimes undefined
 		if (target.creepCarriers === undefined) {
 			target.creepCarriers = [];
