bf1cdea552b39530154a83643e9adb080949feee
Carson McManus <carson.mcmanus1@gmail.com>
2024-01-02 21:42:40 -0500

    add tests for task assignment

--- test/unit/task-assigner.test.ts
+++ test/unit/task-assigner.test.ts
@@ -1,6 +1,6 @@
-import { Assignable, NaiveTaskAssigner, Ordering, PairingComparator } from "utils/task-assigner";
-import { assert } from "chai";
+import { Assignable, NaiveTaskAssigner, Ordering, OverseerTaskAssigner, PairingComparator } from "utils/task-assigner";
 import { Worker, WorkerTask, WorkerTaskKind } from "roles/role.worker";
+import { assert } from "chai";
 
 class MockWorker implements Assignable<WorkerTask> {
 	private _task: WorkerTask | undefined;
@@ -19,6 +19,23 @@ class MockWorker implements Assignable<WorkerTask> {
 	public forceTask(task: WorkerTask): void {
 		this.task = task;
 	}
+}
+
+function countAssignedTasks<W extends Assignable<WorkerTask>>(workers: W[]): { [kind: number]: number } {
+	const counts: Record<WorkerTaskKind, number> = {
+		[WorkerTaskKind.Upgrade]: 0,
+		[WorkerTaskKind.Build]: 0,
+		[WorkerTaskKind.Repair]: 0,
+		[WorkerTaskKind.Dismantle]: 0,
+		[WorkerTaskKind.Mine]: 0,
+	};
+	for (const worker of workers) {
+		if (!worker.task) {
+			continue;
+		}
+		counts[worker.task.task]++;
+	}
+	return counts;
 }
 
 describe("TaskAssigner", () => {
@@ -56,6 +73,132 @@ describe("TaskAssigner", () => {
 			assert.isDefined(worker.task);
 		}
 	});
+});
+
+describe("OverseerTaskAssigner", () => {
+	const cases: [number, WorkerTask[], Record<WorkerTaskKind, number>][] = [
+		[
+			3,
+			[{ task: WorkerTaskKind.Upgrade, target: "1" as Id<StructureController> }],
+			{
+				[WorkerTaskKind.Upgrade]: 3,
+				[WorkerTaskKind.Build]: 0,
+				[WorkerTaskKind.Repair]: 0,
+				[WorkerTaskKind.Dismantle]: 0,
+				[WorkerTaskKind.Mine]: 0,
+			},
+		],
+		[
+			6,
+			[
+				{ task: WorkerTaskKind.Upgrade, target: "1" as Id<StructureController> },
+				{ task: WorkerTaskKind.Repair, target: "3" as Id<StructureController> },
+				{ task: WorkerTaskKind.Repair, target: "4" as Id<StructureController> },
+			],
+			{
+				[WorkerTaskKind.Upgrade]: 4,
+				[WorkerTaskKind.Build]: 0,
+				[WorkerTaskKind.Repair]: 2,
+				[WorkerTaskKind.Dismantle]: 0,
+				[WorkerTaskKind.Mine]: 0,
+			},
+		],
+		[
+			6,
+			[
+				{ task: WorkerTaskKind.Upgrade, target: "1" as Id<StructureController> },
+				{ task: WorkerTaskKind.Build, target: "2" as Id<StructureController> },
+				{ task: WorkerTaskKind.Repair, target: "3" as Id<StructureController> },
+				{ task: WorkerTaskKind.Repair, target: "4" as Id<StructureController> },
+			],
+			{
+				[WorkerTaskKind.Upgrade]: 1,
+				[WorkerTaskKind.Build]: 5,
+				[WorkerTaskKind.Repair]: 0,
+				[WorkerTaskKind.Dismantle]: 0,
+				[WorkerTaskKind.Mine]: 0,
+			},
+		],
+		[
+			6,
+			[
+				{ task: WorkerTaskKind.Upgrade, target: "1" as Id<StructureController> },
+				{ task: WorkerTaskKind.Repair, target: "3" as Id<StructureController> },
+				{ task: WorkerTaskKind.Build, target: "2" as Id<StructureController> },
+				{ task: WorkerTaskKind.Repair, target: "4" as Id<StructureController> },
+			],
+			{
+				[WorkerTaskKind.Upgrade]: 1,
+				[WorkerTaskKind.Build]: 4,
+				[WorkerTaskKind.Repair]: 1,
+				[WorkerTaskKind.Dismantle]: 0,
+				[WorkerTaskKind.Mine]: 0,
+			},
+		],
+		[
+			3,
+			[
+				{ task: WorkerTaskKind.Upgrade, target: "1" as Id<StructureController> },
+				{ task: WorkerTaskKind.Build, target: "2" as Id<StructureController> },
+			],
+			{
+				[WorkerTaskKind.Upgrade]: 1,
+				[WorkerTaskKind.Build]: 2,
+				[WorkerTaskKind.Repair]: 0,
+				[WorkerTaskKind.Dismantle]: 0,
+				[WorkerTaskKind.Mine]: 0,
+			},
+		],
+	];
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	for (const [casenum, [numWorkers, tasks, _]] of cases.entries()) {
+		it(`should ensure all workers have a task, even if there aren't enough tasks (case ${casenum})`, () => {
+			const workers = [];
+			for (let w = 0; w < numWorkers; w++) {
+				workers.push(new MockWorker());
+			}
+
+			const assigner = new OverseerTaskAssigner(workers, tasks);
+			assigner.assignTasks();
+			const allAssigned = workers.every(w => !!w.task);
+			assert.isTrue(allAssigned);
+		});
+	}
+
+	for (const [casenum, [numWorkers, tasks, expectedCounts]] of cases.entries()) {
+		it(`should assign the expected number of workers (case ${casenum})`, () => {
+			const workers = [];
+			for (let w = 0; w < numWorkers; w++) {
+				workers.push(new MockWorker());
+			}
+
+			const assigner = new OverseerTaskAssigner(workers, tasks);
+			assigner.assignTasks();
+			const gotCounts = countAssignedTasks(workers);
+			assert.deepEqual(gotCounts, expectedCounts);
+		});
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	for (const [casenum, [numWorkers, tasks, _]] of cases.entries()) {
+		it(`should always focus workers on a single build target if there are build tasks (case ${casenum})`, () => {
+			const workers = [];
+			for (let w = 0; w < numWorkers; w++) {
+				workers.push(new MockWorker());
+			}
+
+			const assigner = new OverseerTaskAssigner(workers, tasks);
+			assigner.assignTasks();
+			const uniqueAssignedBuildTargets = new Set<string>();
+			for (const worker of workers) {
+				if (worker.task?.task === WorkerTaskKind.Build) {
+					uniqueAssignedBuildTargets.add(worker.task.target);
+				}
+			}
+			assert.isAtMost(uniqueAssignedBuildTargets.size, 1);
+		});
+	}
 });
 
 describe("PairingComparator", () => {
